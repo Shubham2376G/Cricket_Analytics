af87679a99e772c8e6da116016e2164987538e2d
Shubham Aggarwal <shubham.kittu20@gmail.com>
2025-02-23 11:22:00 +0530

    Fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from scenedetect import SceneManager, open_video, detect, ContentDetector
 from scenedetect.video_splitter import split_video_ffmpeg
-from utils.segment import segment_video
+from utils.segment import segment_video, calculate_ssim, extract_frames, classify_video, classify_all_videos
 from utils.detection import detect
 from utils.pose_estimationv1 import pose_estimationv1
 import os
@@ -10,9 +10,10 @@ print(HOME)
 
 # Rally segmentation
 segment_video("demo/Cric Test2.mp4", "demo/segments_2")
+classify_all_videos("demo/segments_2")
 
 #Player detection: Batsman - Bowler
-detect("demo/segments_2", "demo/segments_2/bounding_boxes")
+detect("demo/segments_2/rally_videos", "demo/segments_2/rally_videos/bounding_boxes")
 
 #Pose estimation: Batsman - Bowler
-pose_estimationv1("demo/segments_2/bounding_boxes/detections.json", "demo/segments_2/rallies/output")
+pose_estimationv1("demo/segments_2/rally_videos/bounding_boxes/detections.json", "demo/output")
